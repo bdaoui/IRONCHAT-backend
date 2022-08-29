077a467880c620e073deb46fabb6c74e1e7690ec
Muhammed <107894012+Bidz42@users.noreply.github.com>
2022-08-29 08:58:09 +0100

    MERN stack app backend

---
+++ connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(
+	process.env.DB,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	},
+	() => {
+		console.log('connected to mongoDB ;)');
+	}
+);
